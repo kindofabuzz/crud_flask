084983548ddb446a74b0f31551de372b7f2ea10d
jason <kindofabuzz@gmail.com>
2025-09-16 05:47:44 -0500

    final

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+
+# Data class ~ Row of data
+class MyTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    complete = db.Column(db.Integer, default=0)
+    created = db.Column(db.DateTime, default=datetime.now())
+
+    def __repr__(self) -> str:
+        return f"Task {self.id}"
+
+
+# Add an item
+@app.route("/", methods=["POST", "GET"])
+def index():
+    if request.method == "POST":
+        current_task = request.form["content"]
+        new_task = MyTask(content=current_task)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            print(f"ERROR: {e}")
+            return f"ERROR: {e}"
+    # See all current tasks
+    else:
+        tasks = MyTask.query.order_by(MyTask.created).all()
+        return render_template("index.html", tasks=tasks)
+
+# Delete an item
+@app.route("/delete/<int:id>")
+def delete(id:int):
+    delete_task = MyTask.query.get_or_404(id)
+    try:
+        db.session.delete(delete_task)
+        db.session.commit()
+        return redirect("/")
+    except Exception as e:
+        return f"ERROR: {e}"
+
+# Edit an item
+@app.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit(id:int):
+    update_task = MyTask.query.get_or_404(id)
+    if request.method == "POST":
+        update_task.content = request.form["content"]
+        try:
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            return f"ERROR: {e}"
+    else:
+        return render_template("edit.html", task=update_task)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
+    host = os.getenv("FLASK_HOST", "0.0.0.0")
+    port = os.getenv("FLASK_PORT", "5000")
+    app.run(debug=True, host=host, port=int(port))

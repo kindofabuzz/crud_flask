b44da782f2d5a1c078143820087bbefb113a78fb
jason@beast <kindofabuzz@gmail.com>
2025-09-21 03:41:08 -0500

    final

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ class MyTask(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(100), nullable=False)
     complete = db.Column(db.Integer, default=0)
-    created = db.Column(db.DateTime, default=datetime.now())
+    created = db.Column(db.DateTime, default=datetime.now)
 
     def __repr__(self) -> str:
         return f"Task {self.id}"
